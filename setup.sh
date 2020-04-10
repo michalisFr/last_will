eb33e227b64fdf7ddba01cb6edd5e0ae7308839c
Michalis Fragkiadakis <michalis.fr@icloud.com>
2020-04-10 14:01:57 +0300

    Set the proper version of Python (3.8) for the virtualenv

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ chmod +x check_in.sh
 chmod +x last_will.py
 chmod +x last_will.sh
 chmod +x schedule_cron.py
-virtualenv venv
+virtualenv -p /usr/bin/python3.8 venv
 source venv/bin/activate
 pip install -r requirements.txt
 ./last_will.py
