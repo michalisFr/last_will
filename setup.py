3461b807445a272e60991f7c487569d609a467cc
Michalis Fragkiadakis <michalis.fr@icloud.com>
2020-12-15 22:21:41 +0200

    Fixed package description

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='last_will',
     packages=['last_will'],
-    version='0.1.8',
+    version='0.1.9',
     license='MIT',
     description='A program to send encrypted sensitive information to a trusted person in case of your untimely demise',
     long_description=long_description,
@@ -16,7 +16,7 @@ setup(
     author='Michalis Fragkiadakis',
     author_email='michalis.fr@icloud.com',
     url='https://github.com/michalisFr/last_will',
-    download_url='https://github.com/michalisFr/last_will/archive/v0.1.8.tar.gz',
+    download_url='https://github.com/michalisFr/last_will/archive/v0.1.9.tar.gz',
     keywords=['encrypted', 'sensitive information', 'failsafe'],
     install_requires=[
         'certifi',
